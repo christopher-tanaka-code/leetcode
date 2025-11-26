33d9e645845fa989ce145c9df140b5e6729d01f1
christopher-tanaka-code <christ5653249@gmail.com>
2025-11-26 18:32:39 -0500

    Problem 115 / python

---
+++ python/115. Distinct Subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        if n > m:
+            return 0
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for ch in s:
+            # go backwards to avoid reusing this char multiple times in same iteration
+            for j in range(n, 0, -1):
+                if ch == t[j - 1]:
+                    dp[j] += dp[j - 1]
+
+        return dp[n]
