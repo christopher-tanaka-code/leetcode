f2e6a4b43324c4fdc07522291fd586f6d675f59d
christopher-tanaka-code <christ5653249@gmail.com>
2025-12-01 21:41:57 -0500

    Problem 56 / python

---
+++ python/56. Merge Intervals.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+
+        intervals.sort(key=lambda x: x[0])  # sort by start
+        merged = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            last_start, last_end = merged[-1]
+            if start <= last_end:  # overlap (including touching endpoints)
+                merged[-1][1] = max(last_end, end)
+            else:
+                merged.append([start, end])
+
+        return merged
